1c12b13570597466de7e3334a9e60335ff5d74a5
hyoin <kjr04205@nate.com>
2023-06-07 20:54:03 +0900

    props 컴포넌트 컨트롤, useState 컴포넌트관리, input 상태관리

--- src/App.js
+++ src/App.js
@@ -1,50 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom";
+import React from 'react';
+import InputSample from './InputSample';
+
 function App() {
   return (
-    <div>
-      <h1>React Router DOM 테스트</h1>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/topics">topics</a></li>
-        <li><a href="/contact">contact</a></li>
-      </ul>
-      <Router>
-
-        <Routes>
-          <Route path="/" element={<Home></Home>}></Route>
-          <Route path="/topics" element={<Topics></Topics>}></Route>
-          <Route path="/contact" element={<Contact></Contact>}></Route>
-        </Routes>
-      </Router>
-    </div>
-
+    <InputSample/>
   );
-}
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-      <p>Home......</p>
-    </div>
-  )
-}
-function Topics() {
-  return (
-    <div>
-      <h2>Topics</h2>
-      <p>Topics......</p>
-    </div>
-  )
-}
-function Contact() {
-  return (
-    <div>
-      <h2>Contact</h2>
-      <p>Contact......</p>
-    </div>
-  )
 }
 
 export default App;
